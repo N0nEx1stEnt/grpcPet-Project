40dff8ab7948d97332a817ac634165a0a2d18173
N0nEx1stEnt <sergio10001@yandex.ru>
2023-06-19 17:02:25 +0400

    Parse

--- main.go
+++ main.go
@@ -1,7 +1,114 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+)
+
+type Product struct {
+	Name   string  `json:"name" csv:"name"`
+	Price  float64 `json:"price" csv:"price"`
+	Rating float64 `json:"rating" csv:"rating"`
+}
 
 func main() {
-	fmt.Println("Test commit...")
+	filePath := "products.json" // Укажите путь к вашему файлу
+
+	// Открытие файла для чтения
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Не удалось открыть файл: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	// Получение расширения файла
+	fileExt := getFileExtension(filePath)
+
+	// Чтение данных из файла
+	var products []Product
+	switch fileExt {
+	case "json":
+		products, err = readJSON(file)
+	case "csv":
+		products, err = readCSV(file)
+	default:
+		fmt.Println("Неподдерживаемый формат файла.")
+		return
+	}
+
+	if err != nil {
+		fmt.Printf("Ошибка при чтении файла: %v\n", err)
+		return
+	}
+
+	// Нахождение самого дорогого продукта
+	sort.SliceStable(products, func(i, j int) bool {
+		return products[i].Price > products[j].Price
+	})
+	mostExpensive := products[0]
+
+	// Нахождение продукта с самым высоким рейтингом
+	sort.SliceStable(products, func(i, j int) bool {
+		return products[i].Rating > products[j].Rating
+	})
+	highestRating := products[0]
+
+	fmt.Printf("Самый дорогой продукт: %s ($%.2f)\n", mostExpensive.Name, mostExpensive.Price)
+	fmt.Printf("Продукт с самым высоким рейтингом: %s (рейтинг %.2f)\n", highestRating.Name, highestRating.Rating)
+}
+
+// Чтение данных из JSON-файла
+func readJSON(file io.Reader) ([]Product, error) {
+	var products []Product
+	err := json.NewDecoder(file).Decode(&products)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+// Чтение данных из CSV-файла
+func readCSV(file io.Reader) ([]Product, error) {
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 3
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var products []Product
+	for _, record := range records {
+		price, err := strconv.ParseFloat(record[1], 64)
+		if err != nil {
+			return nil, err
+		}
+
+		rating, err := strconv.ParseFloat(record[2], 64)
+		if err != nil {
+			return nil, err
+		}
+
+		product := Product{
+			Name:   record[0],
+			Price:  price,
+			Rating: rating,
+		}
+
+		products = append(products, product)
+	}
+
+	return products, nil
+}
+
+// Получение расширения файла
+func getFileExtension(filePath string) string {
+	return filepath.Ext(filePath)[1:]
 }
